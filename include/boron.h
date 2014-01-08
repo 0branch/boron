7a3f3ff6c34af40631d12eebd78f81b5ba4e8f2d
Karl Robillard <wickedsmoke@users.sf.net>
2014-01-07 21:21:46 -0800

    Bump version.

--- include/boron.h
+++ include/boron.h
@@ -23,8 +23,8 @@
 #include "urlan.h"
 
 
-#define BORON_VERSION_STR  "0.2.8"
-#define BORON_VERSION      0x000208
+#define BORON_VERSION_STR  "0.2.9"
+#define BORON_VERSION      0x000209
 
 
 enum BoronDataType
