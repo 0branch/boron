0branch/boron

commit 48f223b1413cc504d8c13416d93123fa059cf02f

GL draw_prog: Use Vertex Array Objects for all draw setup.
 - Remove glEnv.globalVAO and ClientState tracker.
 - Add DPCompileContext.
 - gui: Update choice, lineedit, list, & menu widgets to use VAOs.

NOTE: widget_itemview.c has not been updated.

gl/draw_prog.h
@@ -2,7 +2,7 @@
 #define DRAW_PROG_H
 /*
   Boron OpenGL Draw Program
-  Copyright 2008-2010 Karl Robillard
+  Copyright 2008-2020 Karl Robillard
 
   This file is part of the Boron programming language.
 
@@ -38,6 +38,19 @@ DPBuffer;
 
 typedef struct
 {
+    GLuint  shaderProg;
+    GLuint  currentVAO;
+    GLuint  guiVAO;
+    GLuint  shapeVAO;
+    GLuint  userVAO;
+}
+DPCompileContext;
+
+
+typedef struct
+{
+    DPCompileContext* ccon;
+    DPCompileContext ccStorage;
     UBuffer  ref;
     UBuffer  inst;
     UBuffer  blocks;
@@ -55,7 +68,6 @@ typedef struct
     UIndex   fontN;       // TexFont binary
     uint32_t indexCount;
     GLuint   indexBuf;
-    GLuint   shaderProg;
     UIndex   shaderResN;
     GLfloat  penX;
     GLfloat  penY;
@@ -82,21 +94,8 @@ struct DrawTextState
 typedef uint16_t    DPSwitch;
 
 
-#define CS_MAX_ATTR     8
-#define CS_MAX_DIVISOR  8
-
-struct ClientState
-{
-    uint16_t attrCount;
-    uint16_t divisorCount;
-    GLuint attr[ CS_MAX_ATTR ];
-    GLuint divisor[ CS_MAX_DIVISOR ];
-};
-
-
 typedef struct
 {
-    struct ClientState client;
     GLuint samplesQueryId;
     GLuint currentProgram;
     TexFont* font;

gl/boron-gl.c
@@ -71,7 +71,6 @@
 #define CREATE_VERSION  0x301
 #else
 #define CREATE_VERSION  0x303
-#define USE_VAO
 #endif
 
 
@@ -576,10 +575,6 @@ CFUNC( uc_handle_events )
             gView = glv_create( CREATE_ATTRIB, CREATE_VERSION );
 #ifdef DEBUG_GL
             enableGLDebug();
-#endif
-#ifdef USE_VAO
-            glGenVertexArrays( 1, &glEnv.globalVAO );
-            glBindVertexArray( glEnv.globalVAO );
 #endif
             gView->user = &glEnv;
             glv_resize( gView, w, h );
@@ -3166,12 +3161,6 @@ cleanup:
     enableGLDebug();
 #endif
 
-#ifdef USE_VAO
-    // Core profile requires use of VAO.
-    glGenVertexArrays( 1, &glEnv.globalVAO );
-    glBindVertexArray( glEnv.globalVAO );
-#endif
-
 #if 0
     {
     GLint nf;
@@ -3260,10 +3249,6 @@ void boron_releaseGL( UThread* ut )
     }
 
     glid_release();
-
-#ifdef USE_VAO
-    glDeleteVertexArrays( 1, &glEnv.globalVAO );
-#endif
 }
 
 

gl/boron-gl.h
@@ -151,7 +151,6 @@ struct GLEnv
     UCell*   guiStyle;          // For GWidget layout & render.
     UIndex   guiArgBlkN;        // For gui_parseArgs() path! results.
     UIndex   guiValueIndex;     // Index of value word in main thread.
-    GLuint  globalVAO;
     UBuffer tmpBin;
     UBuffer tmpStr;
     UBuffer widgetClasses;

gl/gl_types.c
@@ -1193,8 +1193,12 @@ static void fbo_mark( UThread* ut, UCell* cell )
 
 
 /**
-  Usage is the glBufferData usage (e.g. GL_STATIC_DRAW) and used for both
-  the attributes and indicies buffers.
+  Create a vbo! UBuffer with vertices and/or indicies in the currently bound
+  vertex array object.  Use glBindVertexArray(0) prior to this if no specific
+  VAO is desired.
+
+  \param usage  The glBufferData usage (e.g. GL_STATIC_DRAW) and used for
+                both the attributes and indicies buffers.
 
   \return UBuffer index.
 */
@@ -1282,6 +1286,8 @@ int vbo_make( UThread* ut, const UCell* from, UCell* res )
             }
         }
     }
+
+    glBindVertexArray( 0 );
 
     if( ur_is(from, UT_INT) )
     {

gl/widget_choice.c
@@ -20,6 +20,7 @@
 
 
 #include <glv_keys.h>
+#include "glh.h"
 #include "boron-gl.h"
 #include "draw_prog.h"
 #include "os.h"
@@ -270,7 +271,6 @@ static void choice_layout( GWidget* wp )
 
 
     // Compile draw lists.
-
     rc = style + CI_STYLE_CHOICE;
     if( ur_is(rc, UT_BLOCK) )
         ur_compileDP( glEnv.guiUT, rc, 1 );
@@ -281,15 +281,23 @@ static void choice_layout( GWidget* wp )
 
 static void choice_render( GWidget* wp )
 {
+    GLint saveVao;
     EX_PTR;
+
+    // Here we compile & run an isolated draw-prog for the label.
+
+    glGetIntegerv( GL_VERTEX_ARRAY_BINDING, &saveVao );
 
     if( wp->flags & FLAG_UPDATE_LABEL )
     {
         wp->flags &= ~FLAG_UPDATE_LABEL;
+
+        assert( 0 == gDPC );
         choice_updateLabel( glEnv.guiUT, ep );
     }
 
     ur_runDrawProg( glEnv.guiUT, ep->labelDraw );
+    glBindVertexArray( saveVao );
 }
 
 

gl/widget_lineedit.c
@@ -40,6 +40,7 @@ typedef struct
     UIndex   codeN;
     UIndex   filterN;
     UIndex   vboN;          // Text & cursor
+    GLuint   vao;
     uint16_t maxChars;
     uint16_t drawn;
     uint16_t dpSwitch;
@@ -72,30 +73,40 @@ LineEdit;
 #define CURSOR_MOVED    GW_FLAG_USER3
 
 
-static UIndex ledit_vbo( UThread* ut, int maxChars )
+static void ledit_vbo( UThread* ut, LineEdit* ep )
 {
     UBuffer* buf;
     GLuint* gbuf;
     uint16_t* dst;
-    UIndex resN;
+    int maxChars = ep->maxChars;
     int indexCount = 6 * (maxChars + 1);
 
-    resN = ur_makeVbo( ut, GL_DYNAMIC_DRAW,
-                       LEDIT_VERT_IN_BUF(maxChars), NULL,
-                       indexCount, NULL );
-    buf = ur_buffer( resN );
+    glGenVertexArrays( 1, &ep->vao );
+    glBindVertexArray( ep->vao );
+
+    ep->vboN = ur_makeVbo( ut, GL_DYNAMIC_DRAW,
+                           LEDIT_VERT_IN_BUF(maxChars), NULL,
+                           indexCount, NULL );
+    buf = ur_buffer( ep->vboN );
     gbuf = vbo_bufIds(buf);
+
+    glBindBuffer( GL_ARRAY_BUFFER, gbuf[0] );
+    glEnableVertexAttribArray( ALOC_VERTEX );
+    glEnableVertexAttribArray( ALOC_TEXTURE );
+    glVertexAttribPointer( ALOC_VERTEX, 3, GL_FLOAT, GL_FALSE,
+                           LEDIT_ATTR_SIZE, NULL + 8 );
+    glVertexAttribPointer( ALOC_TEXTURE, 2, GL_FLOAT, GL_FALSE,
+                           LEDIT_ATTR_SIZE, NULL + 0 );
+
     glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, gbuf[1] );
-    dst = (uint16_t*)
-    glMapBufferRange( GL_ELEMENT_ARRAY_BUFFER, 0,
-                      sizeof(uint16_t) * indexCount,
-                      GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT );
+    dst = (uint16_t*) glMapBufferRange( GL_ELEMENT_ARRAY_BUFFER, 0,
+                          sizeof(uint16_t) * indexCount,
+                          GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT );
     if( dst )
     {
         vbo_initTextIndices( dst, 0, maxChars + 1 );
         glUnmapBuffer( GL_ELEMENT_ARRAY_BUFFER );
     }
-    return resN;
 }
 
 
@@ -138,9 +149,16 @@ static GWidget* ledit_make( UThread* ut, UBlockIter* bi,
     //ep->state = LEDIT_STATE_DISPLAY;
     ep->strN     = arg[0]->series.buf;
     ep->maxChars = maxChars;
-    ep->vboN     = ledit_vbo( ut, maxChars );       // gc!
+    ledit_vbo( ut, ep );            // gc!
 
     return (GWidget*) ep;
+}
+
+
+void ledit_free( GWidget* wp )
+{
+    EX_PTR;
+    glDeleteVertexArrays( 1, &ep->vao );
 }
 
 
@@ -392,11 +410,12 @@ key_erase:
 
         case GUI_EVENT_WINDOW_CREATED:
             setFlag( CHANGED | NEW_CURSORX );
-            ep->vboN = ledit_vbo( ut, ep->maxChars );
+            ledit_vbo( ut, ep );
             break;
 
         case GUI_EVENT_WINDOW_DESTROYED:
             ep->vboN = 0;
+            ep->vao = 0;
             break;
     }
     return;
@@ -573,12 +592,12 @@ extern void gr_drawText( TexFont* tf, const char* it, const char* end );
 static void ledit_render( GWidget* wp )
 {
     DrawTextState ds;
-    GLuint* buf;
     GLfloat* fdata;
     UBuffer* str;
     TexFont* tf;
     UCell* style = glEnv.guiStyle;
     UThread* ut  = glEnv.guiUT;
+    GLint saveVao;
     int pos;
     EX_PTR;
 
@@ -589,7 +608,6 @@ static void ledit_render( GWidget* wp )
     if( tf )
     {
         str = ur_buffer( ep->strN );
-        buf = vbo_bufIds( ur_buffer(ep->vboN) );
 
         if( flagged( NEW_CURSORX ) )
         {
@@ -619,12 +637,17 @@ static void ledit_render( GWidget* wp )
             }
         }
 
-        glBindBuffer( GL_ARRAY_BUFFER, buf[0] );
+        glGetIntegerv( GL_VERTEX_ARRAY_BINDING, &saveVao );
+        glBindVertexArray( ep->vao );
 
         if( flagged( CHANGED ) )
         {
+            GLuint* gbuf = vbo_bufIds( ur_buffer(ep->vboN) );
+            glBindBuffer( GL_ARRAY_BUFFER, gbuf[0] );
+
             //printf( "KR pos %d,%d\n", ep->editPos, ep->editEnd );
             clrFlag( CHANGED );
+
             fdata = (float*) glMapBufferRange( GL_ARRAY_BUFFER, 0,
                             sizeof(float) * LEDIT_VERT_IN_BUF(ep->maxChars),
                             GL_MAP_WRITE_BIT | GL_MAP_INVALIDATE_BUFFER_BIT );
@@ -643,16 +666,8 @@ static void ledit_render( GWidget* wp )
             }
         }
 
-        glEnableVertexAttribArray( ALOC_TEXTURE );
-
-        glVertexAttribPointer( ALOC_TEXTURE, 2, GL_FLOAT, GL_FALSE,
-                               LEDIT_ATTR_SIZE, NULL + 0 );
-        glVertexAttribPointer( ALOC_VERTEX, 3, GL_FLOAT, GL_FALSE,
-                               LEDIT_ATTR_SIZE, NULL + 8 );
-        glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, buf[1] );
-
         // Set red -2.0 for shader CLUT mode.
-        glUniform4f( ULOC_COLOR, -2.0, 0.0, 0.0, 1.0 ); // glColor4f
+        glUniform4f( ULOC_COLOR, -2.0, 0.0, 0.0, 1.0 );
 
         {
         char* indices = NULL;
@@ -664,7 +679,7 @@ static void ledit_render( GWidget* wp )
         glDrawElements( GL_TRIANGLES, count, GL_UNSIGNED_SHORT, indices );
         }
 
-        glDisableVertexAttribArray( ALOC_TEXTURE );
+        glBindVertexArray( saveVao );
     }
 }
 
@@ -703,7 +718,7 @@ static UStatus ledit_set( UThread* ut, GWidget* wp, const UCell* val )
 GWidgetClass wclass_lineedit =
 {
     "line-edit",
-    ledit_make,         widget_free,        ledit_mark,
+    ledit_make,         ledit_free,         ledit_mark,
     ledit_dispatch,     ledit_sizeHint,     ledit_layout,
     ledit_render,       ledit_select,       ledit_set,
     0, 0

gl/widget_list.c
@@ -20,6 +20,7 @@
 
 
 #include <glv_keys.h>
+#include "glh.h"
 #include "boron-gl.h"
 #include "draw_prog.h"
 #include "os.h"
@@ -46,6 +47,7 @@ typedef struct
 GList;
 
 #define EX_PTR  GList* ep = (GList*) wp
+#define FLAG_UPDATE_DPROG   GW_FLAG_USER1
 
 
 /*-wid-
@@ -325,6 +327,14 @@ static void listw_sizeHint( GWidget* wp, GSizeHint* size )
 
 static void listw_layout( GWidget* wp )
 {
+    EX_PTR;
+    listw_calcMetrics( glEnv.guiUT, ep, 1 );
+    wp->flags |= FLAG_UPDATE_DPROG;
+}
+
+
+static void listw_updateList( UThread* ut, GList* ep )
+{
     UCell* rc;
     UCell* it;
     const UBuffer* blk;
@@ -332,16 +342,12 @@ static void listw_layout( GWidget* wp )
     int col, colCount;
     int itemY;
     UCell* style = glEnv.guiStyle;
-    UThread* ut  = glEnv.guiUT;
     UIndex strN = 0;
-    EX_PTR;
     DPCompiler* save;
     DPCompiler dpc;
 
 
-    listw_calcMetrics( ut, ep, 1 );
-
-    itemY = wp->area.y + wp->area.h - ep->itemHeight;
+    itemY = ep->wid.area.y + ep->wid.area.h - ep->itemHeight;
 
 
     // Compile draw list for visible items.
@@ -354,7 +360,7 @@ static void listw_layout( GWidget* wp )
     rc = style + CI_STYLE_LIST_BG;
     if( ur_is(rc, UT_BLOCK) )
     {
-        gui_initRectCoord( style + CI_STYLE_AREA, wp, UR_ATOM_RECT );
+        gui_initRectCoord( style + CI_STYLE_AREA, &ep->wid, UR_ATOM_RECT );
         ur_compileDP( ut, rc, 1 );
     }
 
@@ -376,7 +382,7 @@ static void listw_layout( GWidget* wp )
 
             rc = style + CI_STYLE_AREA;
             ur_initCoord(rc, 4);
-            rc->coord.n[0] = wp->area.x + (col * ep->colWidth);
+            rc->coord.n[0] = ep->wid.area.x + (col * ep->colWidth);
             rc->coord.n[1] = itemY;
             rc->coord.n[2] = ep->colWidth;
             rc->coord.n[3] = ep->itemHeight;
@@ -422,7 +428,7 @@ static void listw_layout( GWidget* wp )
 
                 rc = style + CI_STYLE_AREA;
                 ur_initCoord(rc, 4);
-                rc->coord.n[0] = wp->area.x + (col * ep->colWidth);
+                rc->coord.n[0] = ep->wid.area.x + (col * ep->colWidth);
                 rc->coord.n[1] = itemY;
                 rc->coord.n[2] = ep->colWidth;
                 rc->coord.n[3] = ep->itemHeight;
@@ -443,9 +449,21 @@ static void listw_layout( GWidget* wp )
 
 static void listw_render( GWidget* wp )
 {
+    GLint saveVao;
     EX_PTR;
 
+    glGetIntegerv( GL_VERTEX_ARRAY_BINDING, &saveVao );
+
+    if( wp->flags & FLAG_UPDATE_DPROG )
+    {
+        wp->flags &= ~FLAG_UPDATE_DPROG;
+
+        assert( 0 == gDPC );
+        listw_updateList( glEnv.guiUT, ep );
+    }
+
     ur_runDrawProg( glEnv.guiUT, ep->dp[0] );
+    glBindVertexArray( saveVao );
 }
 
 
@@ -503,7 +521,7 @@ GWidgetClass wclass_list =
     listw_make,         widget_free,        listw_mark,
     listw_dispatch,     listw_sizeHint,     listw_layout,
     listw_render,       listw_select,       listw_set,
-    0, GW_CONTEXT_MENU
+    0, GW_CONTEXT_MENU | FLAG_UPDATE_DPROG
 };
 
 

commit ff94636128eb177d363bddeb3d2fd78d68c100a6

GL draw_prog: Fix shape commands (broken in commit 48f223b1).

gl/draw_prog.c
@@ -973,7 +973,7 @@ static void emitGeoPrim( const Primitives* it, DPCompiler* emit )
 }
 
 
-static void emitGeoPrims( const Geometry* geo, DPCompiler* emit )
+static void dp_geoFlushPrims( DPCompiler* emit, Geometry* geo )
 {
     Primitives* it  = (Primitives*) geo->prim.ptr.v;
     Primitives* end = it + geo->prim.used;
@@ -982,14 +982,7 @@ static void emitGeoPrims( const Geometry* geo, DPCompiler* emit )
         emitGeoPrim( it, emit );
         ++it;
     }
-}
-
-
-static void compileGeo( const Geometry* geo, DPCompiler* emit )
-{
-    geo_bufferData( geo );
-    emitGeoBind( geo, emit );
-    emitGeoPrims( geo, emit );
+    geo->prim.used = 0;
 }
 
 
@@ -1326,30 +1319,6 @@ void dp_drawTextCell( DPCompiler* emit, UThread* ut, const UCell* cell,
 }
 
 
-static void _sphere( DPCompiler* emit, float radius, int slices, int stacks,
-                     const char* attrib, int inside )
-{
-    Geometry geo;
-
-    geo_init( &geo, attrib, 0, 0, 0 );
-    geo_sphere( &geo, radius, slices, stacks, inside );
-    compileGeo( &geo, emit );
-    geo_free( &geo );
-}
-
-
-static void _box( DPCompiler* emit, const float* minv, const float* maxv,
-                  const char* attrib, int inside )
-{
-    Geometry geo;
-
-    geo_init( &geo, attrib, 0, 0, 0 );
-    geo_box( &geo, minv, maxv, inside );
-    compileGeo( &geo, emit );
-    geo_free( &geo );
-}
-
-
 /*--------------------------------------------------------------------------*/
 
 
@@ -1515,23 +1484,29 @@ static void dp_free( DPCompiler* gpc )
     ur_arrFree( &gpc->primRec );
 
     geo_free( &gpc->tgeo );
+    geo_free( &gpc->sgeo );
 }
 
 
 void dp_tgeoInit( DPCompiler* emit )
 {
-    UBuffer* attr = &emit->tgeo.attr;
+    Geometry* geo = &emit->tgeo;
+    UBuffer* attr = &geo->attr;
     if( ur_testAvail(attr) )
         return;
-    geo_init( &emit->tgeo, "tv", 32, 32, 8 );
-    emitGeoBind( &emit->tgeo, emit );
+    geo_init( geo, "tv", 32, 32, 8 );
+    emitGeoBind( geo, emit );
 }
 
 
-static void dp_tgeoFlushPrims( DPCompiler* emit )
+void dp_sgeoInit( DPCompiler* emit )
 {
-    emitGeoPrims( &emit->tgeo, emit );
-    emit->tgeo.prim.used = 0;
+    Geometry* geo = &emit->sgeo;
+    UBuffer* attr = &geo->attr;
+    if( ur_testAvail(attr) )
+        return;
+    geo_init( geo, "ntv", 32, 32, 8 );
+    emitGeoBind( geo, emit );
 }
 
 
@@ -1793,7 +1768,7 @@ image_next:
 image_geo:
                     dp_tgeoInit( emit );
                     geo_image( &emit->tgeo, x, y, x + w, y + h );
-                    dp_tgeoFlushPrims( emit );
+                    dp_geoFlushPrims( emit, &emit->tgeo );
                 }
                 else
                 {
@@ -1996,7 +1971,10 @@ bad_tinst_arg:
 bad_sphere:
                     scriptError( "sphere expected radius slices,stacks" );
                 }
-                _sphere( emit, radius, slices, stacks, "ntv", 0 );
+
+                dp_sgeoInit( emit );
+                geo_sphere( &emit->sgeo, radius, slices, stacks, 0 );
+                dp_geoFlushPrims( emit, &emit->sgeo );
             }
                 break;
 
@@ -2009,7 +1987,9 @@ bad_sphere:
                 INC_PC
                 cellToVec3( pc, box + 3 );
 
-                _box( emit, box, box + 3, "ntv", option );
+                dp_sgeoInit( emit );
+                geo_box( &emit->sgeo, box, box + 3, option );
+                dp_geoFlushPrims( emit, &emit->sgeo );
             }
                 break;
 
@@ -2040,7 +2020,7 @@ bad_quad:
                 dp_tgeoInit( emit );
                 geo_quadSkin( &emit->tgeo, val->coord.n,
                               ts->coord.n, tc->coord.n, skin );
-                dp_tgeoFlushPrims( emit );
+                dp_geoFlushPrims( emit, &emit->tgeo );
             }
                 break;
 
@@ -2702,9 +2682,11 @@ DPHeader* dp_finish( UThread* ut, DPCompiler* emit )
                    (GLuint*) emit->blocks.ptr.i );
     }
 
+    // Transfer accumulated geometry to GPU.
+    glBindVertexArray(0);
     genIndices( ut, emit );
-
     geo_bufferData( &emit->tgeo );
+    geo_bufferData( &emit->sgeo );
 
     // Put header, resource entries, and program bytecode into a single
     // UBuffer.

gl/draw_prog.h
@@ -72,6 +72,7 @@ typedef struct
     GLfloat  penX;
     GLfloat  penY;
     Geometry tgeo;      // Textured geometry.
+    Geometry sgeo;      // Shape geometry with uvs & normals.
 }
 DPCompiler;
 

gl/geo.c
@@ -590,6 +590,7 @@ void geo_sphere( Geometry* geo, float radius, int slices, int stacks,
     float latInc = M_PI / stacks;
     float lonInc = (2.0 * M_PI) / slices;
     int n, s;
+    int astart;
 
 
     n = slices * stacks;
@@ -597,6 +598,7 @@ void geo_sphere( Geometry* geo, float radius, int slices, int stacks,
     if( inside )
         geo->flags |= GEOM_INSIDE;
 
+    astart = geo_vertCount( geo );
     sphereVertex( geo,  0.0, M_PI, 0.0, 0.0,  radius );
     sphereVertex( geo, M_PI, M_PI, 0.0, 0.0, -radius );
 
@@ -617,9 +619,9 @@ void geo_sphere( Geometry* geo, float radius, int slices, int stacks,
     {
         uint16_t* it;
         int ia, ib;
-        int istart = 2;
+        int istart = astart + 2;
 
-        it = geo->idx.ptr.u16;
+        it = geo->idx.ptr.u16 + geo->idx.used;
 
         for( s = 0; s < slices; ++s )
         {
@@ -629,7 +631,7 @@ void geo_sphere( Geometry* geo, float radius, int slices, int stacks,
           //ib = (s == (slices - 1)) ? 2 : ia + 1;
             ib = ia + 1;
 
-            *it++ = 0;
+            *it++ = astart;
             for( n = 1; n < stacks; ++n )
             {
                 *it++ = ia;
@@ -637,7 +639,7 @@ void geo_sphere( Geometry* geo, float radius, int slices, int stacks,
                 ia += slices + 1;
                 ib += slices + 1;
             }
-            *it++ = 1;
+            *it++ = astart + 1;
 
             geo->idx.used += stacks * 2;
 